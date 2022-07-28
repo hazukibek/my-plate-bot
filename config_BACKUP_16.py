5d6e1e2f4f8299fd61355c04ac97b783116fd7e2
Asus <beketaisha2006@gmail.com>
2022-07-28 16:38:25 +0500

    hey

---
+++ config_BACKUP_16.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "5469194652:AAFpE92m4xHpqQVrfpbDXxVsg8QO0zHt9ZM"
+<<<<<<< HEAD
+APP_URL = "https://my-plate-bot.herokuapp.com/" + BOT_TOKEN
+DB_URI = "postgres://wcrummtytesnma:3c9095a35b118827f3bf10fbb64930e80dabf7426fa407e31319c8e7a049b4fd@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d15c59u74eqsf"
+=======
+APP_URL = ""
+>>>>>>> dda810d19b11a02d8a54968a961bc11df4c01552
