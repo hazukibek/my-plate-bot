87f3fad1658d0dc132b2a5e2db7914e53590b323
hazukibek <109910769+hazukibek@users.noreply.github.com>
2022-07-30 01:22:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5469194652:AAFpE92m4xHpqQVrfpbDXxVsg8QO0zHt9ZM"
+BOT_TOKEN = "5469194652:AAFpE92m4xHpqQVrfpbDXxVsg8QO0zHt9Z"
 APP_URL = "https://my-plate-bot.herokuapp.com/" + BOT_TOKEN
